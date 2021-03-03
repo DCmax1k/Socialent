da5b3494bd34d738aebd0d3c557737227259bd39
Dylan Caldwell <62185929+DCmax1k@users.noreply.github.com>
2021-03-03 10:58:34 -0500

    Started transition to firestore for signup.js

--- routes/signup.js
+++ routes/signup.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
+const firebase_admin = require('firebase_admin');
+const db = firebase_admin.firestore();
 
 const User = require('../models/User');
 
@@ -11,26 +13,35 @@ router.get('/', (req, res) => {
 // Create user
 router.post('/', async (req, res) => {
   try {
-    const findUserUsername = await User.find({
-      username: req.body.username,
-    });
+    // const findUserUsername = await User.find({username: req.body.username,});
+    const findUserUsername = (await db.collection('users').where('username', '==', req.body.username).get()).docs.map(doc => doc.ref.data());
     if (findUserUsername.length !== 0) {
       res.json({
         response: 'username taken',
       });
     } else {
-      const createUser = await new User({
+//       const createUser = await new User({
+//         emailData: { email: req.body.email, verified: false },
+//         name: req.body.name,
+//         username: req.body.username,
+//         password: req.body.password,
+//         devices: [req.body.device],
+//       });
+//       const saveUser = await createUser.save();
+      // const newID = 
+      const createUserData = {
+//        _id: newID,
         emailData: { email: req.body.email, verified: false },
         name: req.body.name,
         username: req.body.username,
         password: req.body.password,
         devices: [req.body.device],
-      });
-      const saveUser = await createUser.save();
+      };
+      const createUser = (await db.collection('users').add(createUserData))
       res.json({
         response: 'account created',
-        id: saveUser._id,
-        username: saveUser.username,
+        id: newID,
+        username: req.body.username,
       });
     }
   } catch (err) {
