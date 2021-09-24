099ce4188f28dad7b36bfa803ab48920e7d32fca
Dylan Caldwell <dylancaldwell@Dylans-iMac.lan>
2021-09-23 22:55:55 -0400

    Update removes images from storage when data is delete from db

--- globalFunctions/deleteAccount.js
+++ globalFunctions/deleteAccount.js
@@ -1,48 +1,64 @@
 const firebase_admin = require('firebase-admin');
 const db = firebase_admin.firestore();
+const { getStorage, ref, deleteObject } = require('firebase/storage');
+const storage = getStorage();
 
 const deleteUser = async id => {
     try {
         // const user = await User.findById(id);
         const user = (await db.collection('users').where('_id', '==', id).get()).docs[0].data();
 
-          // Deleting associated posts
-          // const deleteAllPosts = await Post.deleteMany({ 'author._id': user._id });
-          // const deleteIdsFromFollowing = await User.updateMany({
-          //   $pull: { following: user._id },
-          // });
-          const deleteAllPosts = (await db.collection('posts').where('author._id', '==', user._id).get()).docs.forEach(async doc => {
+          // Delete all posts
+          // First delete all posts images in storage
+          (await db.collection('posts').where('author._id', '==', user._id).get()).docs.forEach(post => {
+            if (post.data().url.includes('firebase')) {
+              // Find file name
+              const splitVersion = post.url.split('%2F');
+              const fileName = splitVersion[2].slice(0, splitVersion[2].length - 10);
+              const postsRef = ref(storage, `posts/${user._id}`);
+              const imageRef = ref(postsRef, fileName);
+      
+              deleteObject(imageRef);
+            }
+          });
+          (await db.collection('posts').where('author._id', '==', user._id).get()).docs.forEach(doc => {
             try {
-              const deleteDoc = await doc.ref.delete();
+              doc.ref.delete();
             } catch(err) {
               console.error(err);
             }
           });
-          const usersWithFollwoingIds = (await db.collection('users').where('following', 'array-contains', user._id).get()).docs.forEach(async doc => {
+
+          // Delete users id from everyones following list
+          (await db.collection('users').where('following', 'array-contains', user._id).get()).docs.forEach(async doc => {
             const followingArray = doc.data().following;
             const pos = followingArray.indexOf(user._id);
             followingArray.splice(pos, 1);
             const deleteIdFromFollowing = await doc.ref.update('following', followingArray);
           });
 
-          // Deleting associated conversations if only two people in convo
-          // const allConversations = await Conversation.find();
-          // const conversations = allConversations.map(conversation => {
-          //     if (conversation.people.includes(user._id)) {
-          //         return conversation;
-          //     }
-          // });
-          // conversations.forEach(async conversation => {
-          //     if (conversation != undefined) {
-          //       var deleteConversation = await Conversation.deleteOne({ _id: conversation._id});
-          //     }
-          // });
-          (await db.collection('conversations').where('people', 'array-contains', user._id).get()).docs.forEach(async doc => {
+          // Delete conversaions with them in it when its only them and one other.
+          (await db.collection('conversations').where('people', 'array-contains', user._id).get()).docs.forEach(doc => {
             try {
               if (doc.data().people.length === 2) {
+                // First delete the images from storage in the chat
+                doc.data().messages.forEach(message => {
+                  if (message.type == 'img' && message.value.includes('firebase')) {
+                      // Find file name
+                      const splitVersion = message.value.split('%2F');
+                      const fileName = splitVersion[2].slice(0, splitVersion[2].length - 10);
+                      const messagesRef = ref(storage, `messageImages/${user._id}`);
+                      const imageRef = ref(messagesRef, fileName);
+                
+                      deleteObject(imageRef);
+                  }
+                });
 
                 doc.ref.delete();
                 
+              } else {
+                const people = doc.data().people;
+                doc.ref.update('people', people.filter(id => id !== user._id));
               }
             } catch(err) {
               console.error(err);
@@ -50,7 +66,16 @@ const deleteUser = async id => {
           });
 
           // Delete user
-          // const deleteUser = await User.deleteOne({ _id: user._id });
+          // First delete users profile image in storage
+          if (user.profileImage.includes('firebase')) {
+            // Find file name
+            const splitVersion = post.url.split('%2F');
+            const fileName = splitVersion[2].slice(0, splitVersion[2].length - 10);
+            const postsRef = ref(storage, `profileImgs/${user._id}`);
+            const imageRef = ref(postsRef, fileName);
+    
+            deleteObject(imageRef);
+          }
           const deleteUser = await ( await db.collection('users').where('_id', '==', user._id).get()).docs[0].ref.delete();
           return 'success';
       } catch (err) {
